37544864c3a000ac8aa14aa6be91f8a9d5a5bc58
Jeff S <jpsayre@gmail.com>
2026-04-21 13:36:40 -0300

    Add 5 new photos, center globe on Chicago

--- photos.js
+++ photos.js
@@ -3,5 +3,9 @@ const PHOTOS = [
     { file: "assets/photography/IMG_3686.JPG", title: "", location: "", description: "" },
     { file: "assets/photography/IMG_3112.JPG", title: "", location: "", description: "" },
     { file: "assets/photography/IMG_3044.JPG", title: "", location: "", description: "" },
+    { file: "assets/photography/IMG_1746.JPG", title: "", location: "", description: "" },
+    { file: "assets/photography/IMG_1822.JPG", title: "", location: "", description: "" },
     { file: "assets/photography/896D74DB-8467-4B34-8FA4-A7307EE6EEF4.jpg", title: "", location: "", description: "" },
+    { file: "assets/photography/11728779_10103147077548309_8249503361613831257_o_Original.jpg", title: "", location: "", description: "" },
+    { file: "assets/photography/885590_10103147076969469_1424372816114511424_o_Original.jpg", title: "", location: "", description: "" },
 ];
